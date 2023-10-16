e55955db0581666e86881cca96d659f71bcd53db
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-10-16 21:13:42 +0400

    created project and added @angular/material

---
+++ final-project-angular/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'final-project-angular';
+}
