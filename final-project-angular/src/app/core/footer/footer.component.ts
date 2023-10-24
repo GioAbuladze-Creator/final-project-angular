35d69618b45854a1ac8b76d5b30571a41e2e1b9f
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-10-24 18:28:44 +0400

    added edit profile function checked and works fine

--- final-project-angular/src/app/core/footer/footer.component.ts
+++ final-project-angular/src/app/core/footer/footer.component.ts
@@ -1,14 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
-
+  constructor(
+    public dialog: MatDialog,
+    private auth: AuthService
+  ) { }
+  editUser(enterAnimationDuration: string, exitAnimationDuration: string) {
+    if (this.auth.isAuthorized) {
+      this.dialog.open(SignUpComponent, {
+        width: '350px',
+        enterAnimationDuration,
+        exitAnimationDuration,
+      })
+    }
+  }
 }
--- final-project-angular/src/app/core/sign-up/sign-up.component.ts
+++ final-project-angular/src/app/core/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 
 import { ConfPassValidator } from 'src/app/shared/validators/confirm-password.validator';
 import { UsersService } from 'src/app/shared/services/users.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -20,12 +21,28 @@ import { UsersService } from 'src/app/shared/services/users.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignUpComponent {
-
+  status = 'Become Ubnisi Member';
   constructor(
     private fb: FormBuilder,
     private usersService: UsersService,
     public dialogRef: MatDialogRef<SignUpComponent>,
-  ) { }
+    private auth: AuthService
+  ) {
+    if (this.auth.isAuthorized) {
+      this.status = 'Edit Profile';
+      this.agreement?.setValue(true);
+      this.agreement?.disable();
+      
+      this.form.patchValue({
+        firstname: this.auth.loggedUser?.firstname,
+        lastname: this.auth.loggedUser?.lastname,
+        email: this.auth.loggedUser?.email,
+        phone: this.auth.loggedUser?.phone,
+        password: this.auth.loggedUser?.password,
+        confirmPassword: this.auth.loggedUser?.password,
+      })
+    }
+  }
   public form = this.fb.group({
     firstname: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
     lastname: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
@@ -33,7 +50,7 @@ export class SignUpComponent {
     password: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]{7,}$/)]],
     confirmPassword: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]{7,}$/)]],
     phone: ['', [Validators.required, Validators.pattern(/^\+995\d{9}$/)]],
-    agreement: [false, [Validators.required,]]
+    agreement: [false, [Validators.required]]
   }, { validators: ConfPassValidator });
 
   get firstname() { return this.form.get('firstname'); }
@@ -50,50 +67,77 @@ export class SignUpComponent {
   onSubmit() {
     this.form.markAllAsTouched()
     if (this.form.valid) {
-      this.usersService.findUser(this.email?.value!, this.password?.value!).subscribe(
-        {
-          next: (user) => {
-            if (user) {
-              alert('User already exists')
-            } else {
-              this.usersService.checkUser(this.email?.value!, this.phone?.value!).subscribe({
-                next: (user) => {
-                  if (user) {
-                    alert('User already exists')
-                  } else {
-                    this.usersService.addUser(
-                      {
-                        firstname: this.firstname?.value!,
-                        lastname: this.lastname?.value!,
-                        email: this.email?.value!,
-                        password: this.password?.value!,
-                        phone: this.phone?.value!,
-                        cart: [],
-                        purchased: []
-                      }
-                    ).subscribe(
-                      {
-                        next: () => {
-                          this.form.reset();
-                          setTimeout(() => {
-                            alert('You have successfully registered');
-                            this.form.markAsUntouched();
-                            this.dialogRef.close();
-                          })
-                        },
-                        error: () => alert('Something went wrong')
-                      }
-                    )
-                  }
-                },
-                error: () => alert('Something went wrong')
-              })
-
-            }
-          },
-          error: () => alert('Something went wrong')
+      if(this.auth.isAuthorized) {
+        let newUser={
+          ...this.auth.loggedUser!,
+          firstname: this.firstname?.value!,
+          lastname: this.lastname?.value!,
+          email: this.email?.value!,
+          password: this.password?.value!,
+          phone: this.phone?.value!,
         }
-      )
+        this.usersService.updateUser(newUser).subscribe(
+          {
+            next: () => {
+              this.form.reset();
+              setTimeout(() => {
+                alert('You have successfully updated your profile');
+                this.form.markAsUntouched();
+                this.auth.loggedUser = newUser;
+                this.dialogRef.close();
+              })
+            },
+            error: () => alert('Something went wrong')
+          }
+        )
+      }else{
+
+        this.usersService.findUser(this.email?.value!, this.password?.value!).subscribe(
+          {
+            next: (user) => {
+              if (user) {
+                alert('User already exists')
+              } else {
+                this.usersService.checkUser(this.email?.value!, this.phone?.value!).subscribe({
+                  next: (user) => {
+                    if (user) {
+                      alert('User already exists')
+                    } else {
+                      this.usersService.addUser(
+                        {
+                          firstname: this.firstname?.value!,
+                          lastname: this.lastname?.value!,
+                          email: this.email?.value!,
+                          password: this.password?.value!,
+                          phone: this.phone?.value!,
+                          cart: [],
+                          purchased: []
+                        }
+                      ).subscribe(
+                        {
+                          next: () => {
+                            this.form.reset();
+                            setTimeout(() => {
+                              alert('You have successfully registered');
+                              this.form.markAsUntouched();
+                              this.dialogRef.close();
+                            })
+                          },
+                          error: () => alert('Something went wrong')
+                        }
+                      )
+                    }
+                  },
+                  error: () => alert('Something went wrong')
+                })
+  
+              }
+            },
+            error: () => alert('Something went wrong')
+          }
+        )
+      }
+
 
     }
   }
