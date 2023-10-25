d36bceec6407bfbfa70d29295b03f49f53d23daf
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-10-25 10:12:49 +0400

    saving state when clicking on buynow

--- final-project-angular/src/app/features/single-product/single-product.component.ts
+++ final-project-angular/src/app/features/single-product/single-product.component.ts
@@ -5,12 +5,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+import { Observable, tap } from 'rxjs';
 import { TopBarComponent } from 'src/app/core/top-bar/top-bar.component';
 import { Product } from 'src/app/shared/interfaces/product';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { SalePipe } from 'src/app/shared/pipes/sale.pipe';
 import { CategoryBarComponent } from 'src/app/core/category-bar/category-bar.component';
-import { Observable, tap } from 'rxjs';
 import { CartService } from '../cart/cart.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
@@ -36,14 +36,14 @@ export class SingleProductComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
-    private cartService:CartService,
-    private authService:AuthService,
-    private router:Router,
-    private localstorage:LocalStorageService
+    private cartService: CartService,
+    private authService: AuthService,
+    private router: Router,
+    private localstorage: LocalStorageService
   ) { }
   product$!: Observable<Product>;
   id!: string;
-  stars: { [key: number]: string } ={1:'star_border',2:'star_border',3:'star_border',4:'star_border',5:'star_border'};
+  stars: { [key: number]: string } = { 1: 'star_border', 2: 'star_border', 3: 'star_border', 4: 'star_border', 5: 'star_border' };
   mainImage: string = '';
 
   changeImage(image: string) {
@@ -55,33 +55,38 @@ export class SingleProductComponent implements OnInit {
       this.id = id;
     }
     this.product$ = this.apiService.fetchProduct(this.id).pipe(
-      tap((data:Product)=>{
-        let rating=Math.floor(data.rating)
-        for(let i=1;i<=rating;i++){
-          this.stars[i]='star'
+      tap((data: Product) => {
+        let rating = Math.floor(data.rating)
+        for (let i = 1; i <= rating; i++) {
+          this.stars[i] = 'star'
         }
         this.mainImage = data.images[0];
       })
-      
+
     )
   }
   addToCart(product: Product) {
-    if(!this.authService.isAuthorized){
-      this.localstorage.set('redirectUrl',this.router.url)
+    if (!this.authService.isAuthorized) {
+      this.localstorage.set('redirectUrl', this.router.url)
       this.router.navigate(['/login'])
       return
     }
     this.cartService.addToCart(product);
   }
   buyNow(product: Product) {
-    let cartItem={
-      id:product.id,
-      item:product,
-      quantity:1
+    if(!this.authService.isAuthorized){
+      this.localstorage.set('redirectUrl', this.router.url)
+      this.router.navigate(['/login'])
+      return
+    }
+    let cartItem = {
+      id: product.id,
+      item: product,
+      quantity: 1
     }
     this.router.navigate(['/checkout'], {
       queryParams: { objectData: JSON.stringify(cartItem) },
     });
   }
-  
+
 }
